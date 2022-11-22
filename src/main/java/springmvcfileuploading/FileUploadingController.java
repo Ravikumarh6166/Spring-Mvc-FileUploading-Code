382f96387ee85bfca4cf5c8b4cf9e77963bb0c17
Ravikumar6166 <ravikumar.h@4-serveinc.com>
2022-11-22 10:56:48 +0530

    Spring Mvc FileUploding code

---
+++ src/main/java/springmvcfileuploading/FileUploadingController.java
@@ -0,0 +1,56 @@
+package springmvcfileuploading;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+@Controller
+public class FileUploadingController {
+
+	@RequestMapping("/fileform")
+	public String showUploadForm() {
+		return "fileform";
+
+	}
+
+	@RequestMapping(value = "/uploadfile", method = RequestMethod.POST)
+	public String fileupload(@RequestParam("profile") CommonsMultipartFile file, HttpSession session, Model model) {
+		System.out.println("file upload handller");
+		System.out.println(file.getSize());
+		System.out.println(file.getContentType());
+		System.out.println(file.getName());
+		System.out.println(file.getOriginalFilename());
+		System.out.println(file.getStorageDescription());
+		byte[] data = file.getBytes();
+		String path = session.getServletContext().getRealPath("/") + "WEB-INF" + File.separator + "resources"
+				+ File.separator + "image" + File.separator + file.getOriginalFilename();
+		System.out.println(path);
+		try {
+			FileOutputStream fos = new FileOutputStream(path);
+			fos.write(data);
+			//fos.close();
+			//System.out.println("file uploaded");
+			model.addAttribute("msg","uploaded successfully" );
+			model.addAttribute("filename", file.getOriginalFilename());
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("uploading error");
+			model.addAttribute("msg","uploading error" );
+		}
+		
+		
+		return "filesuccess";
+
+	}
+
+}
